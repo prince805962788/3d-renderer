57fd39aa308773f2e64e0558b459c6b25f77c647
wangziqian <wangziqian@fabu.ai>
2024-11-29 16:02:46 +0800

    feat: complete color buffer

--- src/main.c
+++ src/main.c
@@ -1,41 +1,70 @@
 #include </opt/homebrew/include/SDL2/SDL.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
 bool is_running = false;
+
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
     fprintf(stderr, "Error initializing SDL.\n");
     return false;
   }
-  // Create SDL Window
+  /*
+    use SDL to query fullscreen resolution, but the fullscreen has system header
+    not real fullscreen
+  */
+  // SDL_DisplayMode display_mode;
+  // SDL_GetCurrentDisplayMode(0, &display_mode);
+  // window_width = display_mode.w;
+  // window_height = display_mode.h;
+
+  // create SDL Window
   window =
       SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       800, 600, SDL_WINDOW_BORDERLESS);
+                       window_width, window_height, SDL_WINDOW_BORDERLESS);
   if (!window) {
     fprintf(stderr, "Error creating SDL window.\n");
     return false;
   }
-  // Create SDL Renderer
+  // create SDL Renderer
   renderer = SDL_CreateRenderer(window, -1, 0);
   if (!renderer) {
     fprintf(stderr, "Error creating SDL renderer.\n");
     return false;
   }
+  /*
+   open this, into real fullscreen
+  */
+  // SDL_SetWindowFullscreen(window, SDL_TRUE);
   return true;
 }
-void setup(void) {}
+void setup(void) {
+  // allocate memory for color buffer
+  color_buffer =
+      (uint32_t*)malloc(sizeof(uint32_t) * window_width * window_height);
+  // create color buffer texture
+  color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+                                           SDL_TEXTUREACCESS_STREAMING,
+                                           window_width, window_height);
+}
 void process_input(void) {
   SDL_Event event;
   SDL_PollEvent(&event);
   switch (event.type) {
-    case SDL_QUIT:  // Close window with window close button
+    case SDL_QUIT:  // close window with window close button
       is_running = false;
       break;
-    case SDL_KEYDOWN:  // Close window with escape
+    case SDL_KEYDOWN:  // close window with escape
       if (event.key.keysym.sym == SDLK_ESCAPE) {
         is_running = false;
       }
@@ -43,10 +72,49 @@ void process_input(void) {
   }
 }
 void update(void) {}
+void render_color_buffer(void) {
+  SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer,
+                    (int)window_width * sizeof(uint32_t));
+  SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+void clear_color_buffer(uint32_t color) {
+  for (int y = 0; y < window_height; y++) {
+    for (int x = 0; x < window_width; x++) {
+      color_buffer[(y * window_width) + x] = color;
+    }
+  }
+}
+void draw_grid(void) {
+  for (int y = 0; y < window_height; y += 10) {
+    for (int x = 0; x < window_width; x += 10) {
+      color_buffer[(y * window_width) + x] = 0x333333ff;
+    }
+  }
+}
+void draw_rect(int x, int y, int width, int height, uint32_t color) {
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      int current_x = x + j;
+      int current_y = y + i;
+      color_buffer[(current_y * window_width) + current_x] = color;
+    }
+  }
+}
 void render(void) {
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);  // set color to black
+  SDL_RenderClear(renderer);                       // clear the screen
+
+  draw_grid();                     // render grid
+  render_color_buffer();           // render color buffer result
+  clear_color_buffer(0x000000ff);  // clear color buffer to prepare next frame
+
+  SDL_RenderPresent(renderer);  // render the screen
+}
+void destroy_window(void) {
+  free(color_buffer);             // destroy color buffer
+  SDL_DestroyRenderer(renderer);  // destroy renderer
+  SDL_DestroyWindow(window);      // destroy window
+  SDL_Quit();                     // quit SDL
 }
 int main() {
   is_running = initialize_window();
@@ -56,5 +124,6 @@ int main() {
     update();
     render();
   }
+  destroy_window();
   return 0;
 }
